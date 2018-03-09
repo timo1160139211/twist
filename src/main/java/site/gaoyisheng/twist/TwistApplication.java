9c57cbc7c942ee4c880d7fa3923a3086ddb8f860
timo1160139211 <1160139211@qq.com>
2018-03-09 22:25:03 +0800

    idea初始化springboot

---
+++ src/main/java/site/gaoyisheng/twist/TwistApplication.java
@@ -0,0 +1,12 @@
+package site.gaoyisheng.twist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwistApplication.class, args);
+	}
+}
---
+++ src/test/java/site/gaoyisheng/twist/TwistApplicationTests.java
@@ -0,0 +1,16 @@
+package site.gaoyisheng.twist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
